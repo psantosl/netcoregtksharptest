17c860bc458b40f37af2222b2b7a870f28622360
pablo <pablo@codice.es>
2017-11-21 21:40:05 +0100

    Very easy sample

---
+++ MainWindow.cs
@@ -0,0 +1,31 @@
+using System;
+using Gtk;
+
+
+namespace gtktest
+{
+    class MainWindow : Window
+    {
+
+        int mCounter;
+
+        public MainWindow() : base(WindowType.Toplevel) 
+        {
+            VBox box= new VBox();
+
+
+            Button b = new Button("Click me!");
+
+            box.Add(new Label("Hey boy"));
+            box.Add(b);
+            box.Add(new HBox(true, 10));
+
+            Add(box);
+
+            ShowAll();
+            b.Clicked += (sender, e) => this.Title = mCounter++.ToString();
+
+            this.DeleteEvent += (sender, e) => {Console.WriteLine("good bye"); Application.Quit();};
+        }
+    }
+}
